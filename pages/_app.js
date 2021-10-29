01cf6cfdaf0a2acc9823701c243a9543a547c9d3
brandontor <brandon.tor@hotmail.com>
2021-10-28 23:47:42 -0400

    added auth user state

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { UserContext } from '../lib/context'
 
+import {useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useUserData} from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+
+  const userData = useUserData;
+
   return (
+    <UserContext.Provider value={userData}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </UserContext.Provider>
   )
 }
 
