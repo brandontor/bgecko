5b6ae6004eb1b526cc608daee48e519828771e6e
Brandon Tor <brandon.tor@hotmail.com>
2021-11-11 14:57:03 -0500

    added leaderboard header

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import style from '../styles/Leaderboard.module.css'
+import LeaderboardItem from './LeaderboardItem'
 
 function Leaderboard() {
     return (
         <div className={style.leaderBoardContainer}>
-            
+            <div className={style.leaderBoardHeader}>
+                <p>#</p>
+                <p>Coin</p>
+                <p>Price</p>
+                <p>24h</p>
+                <p>Market Cap</p>
+            </div>
+            <LeaderboardItem></LeaderboardItem>
         </div>
     )
 }
