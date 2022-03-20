2fb4b3646ee81bba83e9465bdb3e8c81813788a2
brandontor <brandon.tor@hotmail.com>
2022-03-20 16:19:20 -0400

    leaderboard row data now being pulled from api and pagination is working with some bugs

--- components/home/leaderboard/Leaderboard.js
+++ components/home/leaderboard/Leaderboard.js
@@ -1,14 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import style from './Leaderboard.module.css'
 import LeaderBoardHeader from './LeaderBoardHeader'
 import LeaderBoardRow from './LeaderBoardRow'
-// import Pagination from '@mui/material/Pagination';
-import Pagination from './Pagination'
+import Pagination from '@mui/material/Pagination';
+// import Pagination from './Pagination'
 
 function Leaderboard({leaderBoardData, loading}) {
     const [currentPage, setCurrentPage] = useState(1)
     const [postsPerPage, setPostsPerPage] = useState(10)
     const [pageNumbers, setPageNumbers] = useState([])
+    const pageRef = useRef()
 
     if (loading) { 
         return <h2>Loading...</h2>
@@ -18,12 +19,22 @@ function Leaderboard({leaderBoardData, loading}) {
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = leaderBoardData.slice(indexOfFirstPost, indexOfLastPost)
     const paginate = (pageNumber) => {
-        setCurrentPage(pageNumber)
+        if (pageNumber.getAttribute("aria-label") === "Go to next page") {
+            console.log('go to next page dude')
+            return setCurrentPage(currentPage + 1)
+        }
+        if (pageNumber.getAttribute("aria-label") === "Go to previous page") {
+            console.log('go to previous page dude')
+            return setCurrentPage(currentPage - 1)
+        }
+        return setCurrentPage(pageNumber.innerText)
     }
 
-    console.log('This is the number of pages if page count is 10',pageNumbers)
-    console.log(Math.ceil(leaderBoardData.length/postsPerPage))
-    console.log('This My Data Boy',leaderBoardData)
+    const Numbers = []
+    for(let i=1; i <= Math.ceil(leaderBoardData.length/postsPerPage); i++) {
+        Numbers.push(i)
+    }
+    console.log(Numbers)
     //leaderBoardData.name
     //.symbol
     //.market_cap
@@ -40,7 +51,8 @@ function Leaderboard({leaderBoardData, loading}) {
                 const params = {name, symbol, market_cap, market_cap_change_percentage_24h, market_cap_rank, image, current_price}
               return <LeaderBoardRow key={index} {...params} ></LeaderBoardRow>
             })}
-            <Pagination totalPosts={leaderBoardData.length} postsPerPage={postsPerPage} paginate={paginate}></Pagination>
+            <Pagination ref={pageRef} page={currentPage} count={Numbers.length} color="primary" onChange={(e)=>{paginate(e.currentTarget)}}></Pagination>
+            {/* <Pagination totalPosts={leaderBoardData.length} postsPerPage={postsPerPage} paginate={paginate}></Pagination> */}
         </div>
     )
 }
