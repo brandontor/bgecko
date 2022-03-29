6d9f394684f6d8e393106d236a55605039827b69
Brandon Tor <brandon.tor@hotmail.com>
2022-03-28 22:12:54 -0400

    search bar added - still need to add functionality

--- components/home/searchbar/SearchBar.js
+++ components/home/searchbar/SearchBar.js
@@ -1,21 +1,37 @@
 import React, {useState} from 'react'
 import style from './SearchBar.module.css'
 import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
+import { InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
-    const [hideSearch, setHideSearch] = useState(false)
+    const [showSearch, setShowSearch] = useState(false)
 
+    const focusHandler = () => {
+      if (showSearch) return
+      setShowSearch(true)
+    } 
+
+    const blurHandler = () => {
+      if (!showSearch) return
+      setShowSearch(false)
+    }
 
   return (
     <div className={style.search}>
-        <TextField color="primary" id="standard-basic" label="Search" variant="standard" sx={{
-    '& .MuiInput-underline:before': { borderBottomColor: '#D8DEE9' },
-    '& .MuiInput-underline:after': { borderBottomColor: '#D8DEE9' },'& .MuiInput-underline:hover:before': { borderBottomColor: '#D8DEE9' },
-  }}></TextField>
+        <TextField onFocus={focusHandler} onBlur={blurHandler}
+          color="primary" id="standard-basic" label="Search" variant="standard" sx={{
+          '& .MuiInput-underline:before': { borderBottomColor: '#D8DEE9' },
+          '& .MuiInput-underline:after': { borderBottomColor: '#D8DEE9' },'& .MuiInput-underline:hover:before': { borderBottomColor: '#D8DEE9' },
+          borderRadius: '5px'
+          }}  InputProps={{
+          endAdornment: <InputAdornment sx={{color: '#D8DEE9'}}position="end"><SearchIcon/></InputAdornment>
+        }}></TextField>
+      {showSearch && 
+        <div className={style.dropDown}>
+          <p>Hello</p>
+        </div>}
     </div>
   )
 }
-// InputProps={{ disableUnderline: true,}}
-
 export default SearchBar
